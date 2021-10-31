159cdd785c27982e0ce8cff5599b8705b73867ae
qnchenry-q <henryqnc@gmail.com>
2021-10-31 15:10:03 -0500

    grouped addresses yaml upload

--- yaml_export.py
+++ yaml_export.py
@@ -1,5 +1,6 @@
 import yaml
 import csv
+import pandas as pd
 
 departments_d = {}
 with open('departments_v2.csv') as csvf:
@@ -13,14 +14,26 @@ with open('departments_v2.csv') as csvf:
   with open('employees_yaml.yaml', 'w') as file:
     file.write(yaml.dump(departments_d))
 
-addresses_d = {}
-with open('adresses_v2.csv') as csvf:
-  reader = csv.DictReader(csvf)
+t_df = pd.read_csv('adresses_v2.csv')
 
-  for rows in reader:
-    key = rows['address_id']
-    addresses_d[key] = rows
+grouped = t_df.groupby(['state', 'city'])
 
+final = t_df.set_index(['state', 'city', 'address_id']).sort_values(['state', 'city','address_id'])
 
-  with open('addresses_yaml.yaml', 'w') as file:
-    file.write(yaml.dump(addresses_d))
+final.to_csv('grouped_adresses.csv')
+
+ad = {}
+with open('grouped_addresses.csv') as csvf:
+    reader = csv.DictReader(csvf)
+    for row in reader:
+        state = row.pop("state")
+        city = row.pop("city")
+        if state not in ad:
+            ad[state] = {city: [{row['address_id']: row['street']}]}
+        else:
+            if city in ad[state]:
+                ad[state][city].append({row['address_id']: row['street']})
+            else:
+                ad[state].update({city: [{row['address_id']: row['street']}]})
+with open('grouped_addresses.yaml', 'w') as file:
+    file.write(yaml.dump(ad))
